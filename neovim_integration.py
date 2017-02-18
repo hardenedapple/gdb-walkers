ed1c94408a8cff923795db8c0dec4bc060aaa961
Matthew Malcomson <hardenedapple@gmail.com>
2017-02-18 11:00:14 +0000

    Improve neovim mark-stack command
    Was getting some missing elements because of the method of calculation.

--- neovim_integration.py
+++ neovim_integration.py
@@ -38,8 +38,46 @@ class MarkStack(gdb.Command):
         frame = gdb.selected_frame()
         nvim = get_nvim_instance()
         for mark in string.ascii_uppercase:
-            curpc = frame.pc()
-            pc_pos = gdb.find_pc_line(int(curpc))
+            # TODO Sometimes strange things are happening:
+            # frame.older().pc() == frame.pc()
+            # frame.older().find_sal().pc == 0 != frame.find_sal().pc
+            # frame.find_sal().line != frame.older().find_sal().line
+            # frame.name() != frame.older().name()
+            #   Example problem is in gdb
+            # #0  c_val_print_array (options=0x7fffffffdde0, original_value=<optimized out>, recurse=<optimized out>, stream=0xf62f40, address=<optimized out>, embedded_offset=0, valaddr=0x172efa0 "u_savecommon", type=<optimized out>) at /home/matthew/share/repos/gdb-source/gdb/c-valprint.c:307
+            # #1  c_val_print (type=<optimized out>, embedded_offset=0, address=<optimized out>, stream=0xf62f40, recurse=<optimized out>, original_value=<optimized out>, options=0x7fffffffdde0) at /home/matthew/share/repos/gdb-source/gdb/c-valprint.c:511
+            # #2  0x0000000000697420 in val_print (type=0x0, type@entry=0x18b3870, embedded_offset=0, address=25901168, address@entry=0, stream=stream@entry=0xf62f40, recurse=recurse@entry=0, val=val@entry=0x1811bd0, options=0x7fffffffde90, language=0x88f0e0 <c_language_defn>) at /home/matthew/share/repos/gdb-source/gdb/valprint.c:1120
+            # #3  0x0000000000558eec in c_value_print (val=0x1811bd0, stream=0xf62f40, options=<optimized out>) at /home/matthew/share/repos/gdb-source/gdb/c-valprint.c:698
+            # #4  0x00000000006273a8 in print_value (val=val@entry=0x1811bd0, fmtp=fmtp@entry=0x7fffffffdfa0) at /home/matthew/share/repos/gdb-source/gdb/printcmd.c:1233
+            # #5  0x000000000062743e in print_command_1 (exp=<optimized out>, voidprint=1) at /home/matthew/share/repos/gdb-source/gdb/printcmd.c:1261
+            # #6  0x0000000000495133 in cmd_func (cmd=0xe85630, args=0xe0d816 "$_function_of(&u_savecommon)", from_tty=1) at /home/matthew/share/repos/gdb-source/gdb/cli/cli-decode.c:1888
+            # #7  0x00000000006837b3 in execute_command (p=<optimized out>, p@entry=0xe0d810 "print $_function_of(&u_savecommon)", from_tty=1) at /home/matthew/share/repos/gdb-source/gdb/top.c:674
+            # #8  0x00000000005b1abc in command_handler (command=0xe0d810 "print $_function_of(&u_savecommon)") at /home/matthew/share/repos/gdb-source/gdb/event-top.c:590
+            # #9  0x00000000005b1d88 in command_line_handler (rl=<optimized out>) at /home/matthew/share/repos/gdb-source/gdb/event-top.c:780
+            # #10 0x00000000005b10fc in gdb_rl_callback_handler (rl=0x18b89f0 "print $_function_of(&u_savecommon)") at /home/matthew/share/repos/gdb-source/gdb/event-top.c:213
+            # #11 0x00000000006c4463 in rl_callback_read_char () at /home/matthew/share/repos/gdb-source/readline/callback.c:220
+            # #12 0x00000000005b103e in gdb_rl_callback_read_char_wrapper_noexcept () at /home/matthew/share/repos/gdb-source/gdb/event-top.c:175
+            # #13 0x00000000005b10a9 in gdb_rl_callback_read_char_wrapper (client_data=<optimized out>) at /home/matthew/share/repos/gdb-source/gdb/event-top.c:192
+            # #14 0x00000000005b15d0 in stdin_event_handler (error=<optimized out>, client_data=0xd95c10) at /home/matthew/share/repos/gdb-source/gdb/event-top.c:518
+            # #15 0x00000000005b044d in gdb_wait_for_event (block=block@entry=0) at /home/matthew/share/repos/gdb-source/gdb/event-loop.c:859
+            # #16 0x00000000005b0587 in gdb_do_one_event () at /home/matthew/share/repos/gdb-source/gdb/event-loop.c:322
+            # #17 0x00000000005b0725 in gdb_do_one_event () at /home/matthew/share/repos/gdb-source/gdb/common/common-exceptions.h:221
+            # #18 start_event_loop () at /home/matthew/share/repos/gdb-source/gdb/event-loop.c:371
+            # #19 0x0000000000602fb8 in captured_command_loop (data=data@entry=0x0) at /home/matthew/share/repos/gdb-source/gdb/main.c:325
+            # #20 0x00000000005b2693 in catch_errors (func=func@entry=0x602f90 <captured_command_loop(void*)>, func_args=func_args@entry=0x0, errstring=errstring@entry=0x832059 "", mask=mask@entry=RETURN_MASK_ALL) at /home/matthew/share/repos/gdb-source/gdb/exceptions.c:236
+            # #21 0x0000000000603eae in captured_main (data=0x7fffffffe280) at /home/matthew/share/repos/gdb-source/gdb/main.c:1148
+            # #22 gdb_main (args=args@entry=0x7fffffffe3a0) at /home/matthew/share/repos/gdb-source/gdb/main.c:1158
+            # #23 0x000000000040ec95 in main (argc=<optimized out>, argv=<optimized out>) at /home/matthew/share/repos/gdb-source/gdb/gdb.c:32
+            #
+            # Find out why.
+            #   (maybe something to do with tail-call optimisation?)
+            # This is why I use frame.find_sal() instead of
+            # gdb.find_pc_line(frame.pc())
+            #   (that and it's a cleaner way to reference it)
+
+            # Here we mark c_val_print_array() and c_val_print() in the same
+            # position.
+            pc_pos = frame.find_sal()
             # Can only add this position if we know the filename.
             # If we don't know the filename, clear this mark.
             # If we didn't clear the mark, then neovim would end up with
@@ -115,7 +153,7 @@ class GoHere(gdb.Command):
 
         nvim.command('{} +{} {}'.format(open_method, pos.line,
                                         os.path.abspath(pos.symtab.filename)))
-        nvim.command('{}foldopen!'.format(pos.line))
+        nvim.command('silent! {}foldopen!'.format(pos.line))
 
 
 class ShowHere(gdb.Command):
