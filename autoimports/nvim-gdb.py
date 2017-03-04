0ab311874c47b7e54bb45cf4ae251ba1e676347d
Matthew Malcomson <hardenedapple@gmail.com>
2017-03-04 18:44:01 +0000

    Automatically load neovim data structure walkers
    Have to roll my own loader instead of using the gdb auto-load feature
    so as to match on basename instead of full pathname.

---
+++ autoimports/nvim-gdb.py
@@ -0,0 +1,1 @@
+import neovim_walkers
--- basic-config.py
+++ basic-config.py
@@ -1,13 +1,36 @@
 import gdb
 import os
 import sys
+import importlib
 
 if os.getenv('TERM') == 'dumb':
     gdb.execute('set pagination off')
 
+
 confdir = os.path.expanduser('~/.config/gdb')
 if confdir not in sys.path:
     sys.path.append(confdir)
+del confdir
 
-gdb.execute('add-auto-load-safe-path {}/autoloads'.format(confdir))
-gdb.execute('add-auto-load-scripts-directory {}/autoloads'.format(confdir))
+def importer(event):
+    '''Emulates gdb auto-load scripts-directory but matches on basename.
+
+    The provided auto-load functionality requires a full pathname match, so
+    that a script to be loaded on debugging /full/path/to/debug/program must be
+    placed in autoload-directory/full/path/to/debug/program-gdb.extension .
+
+    This function means that you just need to create a file
+    autoload-directory/program-gdb.py.
+
+    '''
+    progname = event.new_objfile.filename
+    matchname = os.path.basename(progname) + '-gdb.py'
+    # If the file doesn't exist, gdb just prints an error and does nothing.
+    # Asking to get the output as a string via the final 'True' argument means
+    # this error isn't shown to the user.
+    confdir = os.path.expanduser('~/.config/gdb')
+    gdb.execute('source {}/autoimports/{}'.format(confdir, matchname),
+                False, True)
+
+
+gdb.events.new_objfile.connect(importer)
