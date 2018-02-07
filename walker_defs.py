hardenedapple/gdb-walkers

commit b91aff61ca0dc98c1d268bebc140057e2c3bb413

walker_defs.Array() typename now type of elements
Previously the name had to include the '*' so that the type was a
pointer to an element.
This had tripped me up already, and tripped me up again.
Hence I believe it's more intuitive to pass the type of the element.

walker_defs.py
@@ -358,7 +358,7 @@ class Array(walkers.Walker):
 
     def __init__(self, first, start, count, typename, element_size):
         self.first = first
-        self.typename = typename
+        self.typename = typename + '*'
         self.element_size = element_size
         if first:
             self.start_addr = start
@@ -376,12 +376,14 @@ class Array(walkers.Walker):
 
         def __noauto(type_arg):
             element_size = find_type_size(type_arg)
-            typename = type_arg + '*'
+            typename = type_arg
             return typename, element_size
 
         def __first_auto(start_expr, count_expr, _):
             count, start_ele = __first(count_expr, start_expr)
             start_addr, typename = start_ele.v, start_ele.t
+            typename = typename.rstrip()
+            typename = typename[:-1] if typename[-1] == '*' else typename
             return (start_addr, count, typename,
                     gdb.parse_and_eval(start_expr).type.target().sizeof)
 

commit 7c5b7c2d49ba9cb07072ab359b9bba55d43ed571

Fixed `array auto; ...` bug introduced in b91aff6
When `array auto; ...` is used in subsequent steps of a pipeline, it
records the fact that it doesn't know the type of elements it's using by
setting `self.typename` to `None`.

When we switched from giving the array walker the type of pointers to
elements in the array to giving the walker the type of elements in the
array the `__init__` function was updated to modify `self.typename` by
adding a `*` to the end.
This didn't take into account that `self.typename` could be `None`.

This commit adds a conditional to that operation.
I'm a little annoyed that this problem was caught by my test suite.
That means I didn't bother to run the testsuite against my change, or at
least didn't look into the failures.

walker_defs.py
@@ -358,7 +358,7 @@ class Array(walkers.Walker):
 
     def __init__(self, first, start, count, typename, element_size):
         self.first = first
-        self.typename = typename + '*'
+        self.typename = typename + '*' if typename else None
         self.element_size = element_size
         if first:
             self.start_addr = start